50696ae4112522ed45fc23e8bb1b4b232728a68c
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-09-05 21:01:06 +0800

    Update links.js

--- src/data/links.js
+++ src/data/links.js
@@ -6,25 +6,25 @@ const FRIENDLY_LINKS = {
    ['绿色导航', 'https://煐毳誩v.greendh.org/喆r甦'],
   ['找AV导航', 'https://4pmycp.zavdh18.qpon/zapat9kp'],
   ['花小猪导航', 'https://棽蹵祎.9qqq.cc/粺锳'],
-  ['妖涩梨导航', 'https://嗷嗷嗷嗷.yaosl1.sbs/?mark=Q1TN3T'],
-  ['娱乐青导航', 'http://819ylq.yuleqing17.cc/app/Mf4viP'],
-  ['红浪漫导航', 'http://819llm.hlmhs3.top/app/gcbfcV'],
-  ['双马尾导航', 'http://819smw.smaw3.top/app/TtgjWr'],
-  ['东方色导航', 'http://819dong.dfswh3.top/app/Af2z8v'],
-  ['冈本导航', 'http://614ben.gangben3.top/app/0BU7ve'],
-  ['荔枝导航', 'http://627lizhii.lizhidh3.top/app/tosXlA']
+  ['妖涩梨导航', 'https://嗷嗷嗷嗷嗷.yaosl1.sbs/?mark=AMUDEQ'],
+  ['娱乐青导航', 'http://905ylq.yuleqing17.cc/app/9jwNrQ'],
+  ['红浪漫导航', 'http://905llm.hlmhs3.top/app/FRXmEu'],
+  ['双马尾导航', 'http://905smw.smaw3.top/app/F1wRz0'],
+  ['东方色导航', 'http://905dong.dfswh3.top/app/avlPA5'],
+  ['冈本导航', 'http://905ben.gangben4.top/app/4Zef7t'],
+  ['荔枝导航', 'http://905lizhi.lizhidh4.top/app/XjCDt3']
   ],
   'xn--xn--ifia-5058c.haijav.top': [
     ['蓝色导航', 'https://nzru.bluedh63.loan/cqyhw37'],
   ['蓝导航', 'https://3pssi.landh17.party/nvrdcdl'],
   ['福利嫂导航', 'https://顺.flsgo.cc/世界和平.html'],
-  ['6DD导航', 'http://6146d.5ddd6.top/app/f6nEP9'],
-  ['红颜导航', 'http://614hy.hongyan2.top/app/O18VPF'],
-  ['柳如烟导航', 'http://508lry.lrydd.top/app/psEPAJ'],
-  ['22点导航', 'http://ws10d511.ws10d1.top/app/PITIMm'],
-  ['哇嘎入口', 'http://625wgrk.wgrk2.top/app/0UDKsv'],
-  ['3838导航', 'http://62338.3838dh1.top/app/FzsCgx'],
-  ['大丈夫导航', 'http://627heping.dazf2.top/app/dN3vzo']
+  ['6DD导航', 'http://9056dd.6ddd6.top/app/yWwBfN'],
+  ['红颜导航', 'http://905hy.hongyan3.top/app/MTuLXt'],
+  ['柳如烟导航', 'http://905lry.lrydd2.cc/app/5jxu9W'],
+  ['22点导航', 'http://ws10d905.ws10d3.cc/app/Czk3Zf'],
+  ['哇嘎入口', 'http://905wgrk.wgrk3.cc/app/6xqcRh'],
+  ['3838导航', 'http://90538.3838dh2.top/app/i5lwv4'],
+  ['大丈夫导航', 'http://905heping.dazf3.top/app/gohQUh']
   ],
 
   // Cloudflare Pages 预览域名统一使用默认友情链接。
